526a3ce442a5f61874726e07384f70e8496f7add
Arto Bendiken <arto@bendiken.net>
2020-05-19 20:56:26 +0300

    Added a dependency on Flutter's WebView.

--- test/model_viewer_test.dart
+++ test/model_viewer_test.dart
@@ -1,6 +1,6 @@
 /* This is free and unencumbered software released into the public domain. */
 
-import 'package:test/test.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 import 'package:model_viewer/model_viewer.dart';
 
