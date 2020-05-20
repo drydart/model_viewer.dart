6603f9e1d5d3555635bb5da4a2445812af94e437
Arto Bendiken <arto@bendiken.net>
2020-05-20 07:41:00 +0300

    Simplified the example app.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -5,28 +5,16 @@ import 'package:model_viewer/model_viewer.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  Widget build(final BuildContext context) {
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Model Viewer"),
-        ),
+        appBar: AppBar(title: Text("Model Viewer")),
         body: ModelViewer(
           backgroundColor: Color.fromARGB(0xFF, 0xEE, 0xEE, 0xEE),
           src: 'https://modelviewer.dev/shared-assets/models/Astronaut.glb',
-          alt: 'A 3D model of an astronaut',
+          alt: "A 3D model of an astronaut",
           ar: true,
           autoRotate: true,
           cameraControls: true,
