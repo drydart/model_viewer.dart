e20d9d4abc78f7ef9856e33a3e33acf0ec4fd08f
Arto Bendiken <arto@bendiken.net>
2020-07-21 19:50:26 +0300

    Fixed `flutter analyze` warnings and applied `flutter format`.

--- test/model_viewer_test.dart
+++ test/model_viewer_test.dart
@@ -1,8 +1,8 @@
 /* This is free and unencumbered software released into the public domain. */
 
-import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_test/flutter_test.dart'; // ignore_for_file: unused_import
 
-import 'package:model_viewer/model_viewer.dart';
+import 'package:model_viewer/model_viewer.dart'; // ignore_for_file: unused_import
 
 void main() {
   // TODO
